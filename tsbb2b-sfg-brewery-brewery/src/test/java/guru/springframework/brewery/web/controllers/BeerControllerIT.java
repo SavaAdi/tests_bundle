c37ff241bbcee2df721f8ca1b9a51b7331f52702
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-08-01 22:25:41 +0300

    added restTemplate in integration test

---
+++ tsbb2b-sfg-brewery-brewery/src/test/java/guru/springframework/brewery/web/controllers/BeerControllerIT.java
@@ -0,0 +1,23 @@
+package guru.springframework.brewery.web.controllers;
+
+import guru.springframework.brewery.web.model.BeerPagedList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BeerControllerIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate; // this is for servlet/MVC not reactive; autoconfigured for that random port
+
+    @Test
+    void testListBeers() {
+        BeerPagedList beerPagedList = restTemplate.getForObject("/api/v1/beer", BeerPagedList.class);
+
+        assertThat(beerPagedList.getContent()).hasSize(3);
+    }
+}
